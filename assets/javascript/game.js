526f6bbcd6337de9d9650a7fc198e12416b44cce
Ben Fung <benfung1996@gmail.com>
2020-02-09 21:52:08 -0800

    pseudocode
    adjusted html and js. created pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,146 @@
-var word_list = ["Corgi", "French Bull", "Husky", "Beagle", "German Shepherd"]
+// create words to guess
+var possibleWords = ["Corgi", "French Bull", "Husky", "Beagle", "German Shepherd"];
 
+// create variables to store input
+var guessedLetters = [];
+var guessingWord = [];
+var usedGuessingwWords = [];
+var wordToMatch;
+var numGuess = 7;
 var wins = 0;
-var guesses_left = 5;
-
-var random_word = [];
-var blank = [];
-var word_solved = [];
-var current_word = [];
-var remaining_guesses = [];
-var letters_guessed = [];
+var pause = false; 
 
 
+// create function to start game
+function initializeGame() {
 
-
-
-function start() {
-    random_word = words[Math.floor(Math.random() * words.length)];
-    
-    for (var i = 0; i < random_word.length; i++) {
-        if (random_word[i] === " ") {
-            blank.push(" ")
-        } 
-        else {
-            blank.push("_")
-        }
-    }
-    updateDisplay();
-};
-
-function reset() {
-    if (word_solved.length === word_list.length) {
-        word_solved = []
-        wins = 0
-    }
+  // choice random word from word list and turn to uppercase
+  wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase();
+ 
+  // set a for loop to replace each letter of the selected word with " " or " _ "
+  for (var i=0; i < wordToMatch.length; i++){
+  
+    if (wordToMatch[i] === " ") {
+      guessingWord.push(" ")
+    } 
     else {
-        document.getElementById("welcome");
-        random_word = word_list[Math.floor(Math.random() * word_list.length)];
-        console.log(random_word)
-        if (word_solved.includes(random_word) === true) {
-            reset();
-        }
-
-        current_word = [];
-        letters_guessed_text = [];
-
-        for (var i = 0; i < random_word.length; i++) {
-            if (random_word[i] === " ") {
-                blank.push(" ")
-            } 
-            else {
-                blank.push("_")
-            }
-        }
-        updateDisplay();
+      guessingWord.push(" _ ");
     }
+  }
+
+  // refresh scores
+  updateDisplay();
 };
 
-function updateDisplay() {
-  document.getElementById("wins-text").innerHTML = wins;
-  document.getElementById("current_word").innerHTML = current-word.join("");
-  document.getElementById("remaining_guesses").innerHTML = remaining-guesses;
-  document.getElementById("letters_guessed").innerHTML = letters-guessed.join("");
+// function to reset game 
+function resetGame() {
+
+  // if all words from list have been guessed, reset data after 3 second
+  if (usedGuessingwWords.length === possibleWords.length) {
+    usedGuessingwWords = []
+    wins = 0
+    setTimeout(resetGame, 3000); 
+  }
+
+  // if not all words have been guessed, continue to pick new word and console.log it
+  else {
+    pause = false;
+    wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase();
+    console.log(wordToMatch)
+
+    // if the chosen word has been guessed, repeat reset function for another word
+    if (usedGuessingwWords.includes(wordToMatch) === true) {
+      resetGame();
+    }
+    
+    // reset below variables for new word
+    numGuess = 7;
+    guessedLetters = [];
+    guessingWord = [];
+
+
+    // set a for loop to replace each letter of the selected word with " " or " _ "
+    for (var i=0; i < wordToMatch.length; i++){
+  
+      if (wordToMatch[i] === " ") {
+        guessingWord.push(" ")
+      } 
+      else {
+        guessingWord.push("_");
+      }
+    }
+
+    // refresh scores 
+    updateDisplay();
+  }
 };
 
+// function to refresh variables from html text
+function updateDisplay () {
+  document.getElementById("totalWins").innerText = wins;
+  // .join("") allow letter to connect wihtout "," in between
+  document.getElementById("currentWord").innerText = guessingWord.join("");
+  document.getElementById("remainingGuesses").innerText = numGuess;
+  document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ");
+};
+
+// when user pressed a key, function will trigger event
 document.onkeydown = function(event) {
 
-    if (isLetter(event.key)) {
-        checkForLetter(event.key.toLocaleUpperCase());
-    }
+  // if user entered a letter and game is not pause
+  if (isLetter(event.key) && pause === false) {
 
-    document.getElementById("welcome");
+  // set letter to uppercase
+  checkForLetter(event.key.toUpperCase());
+  }
 };
 
+// function to check if the pressed key is a letter from a - z
+var isLetter = function(ch){
+  return typeof ch === "string" && ch.length === 1
+  && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+};
 
+//function that checks if letter is in the guessing word
 function checkForLetter(letter) {
-    var foundLetter = false;
-  
-    for (var i=0; i < random_word.length; i++) {
-      if (letter === random_word[i]) {
-        current_word[i] = letter
-        foundLetter = true
-       
-        if (current_word.join("") === random_word) {
-          wins++
-          word_solved.push(random_word)
-          console.log(word_solved)
-          updateDisplay();
-        }
+  var foundLetter = false;
+
+
+  for (var i=0; i < wordToMatch.length; i++) {
+    if (letter === wordToMatch[i]) {
+      guessingWord[i] = letter
+      foundLetter = true
+
+      if (guessingWord.join("") === wordToMatch) {
+        wins++
+        usedGuessingwWords.push(wordToMatch)
+        console.log(usedGuessingwWords)
+        pause = true;
+        updateDisplay();
+        setTimeout(resetGame, 2000);
       }
     }
-
-    if (foundLetter === false) {
-    
-      if (letters_guessed.includes(letter) === false) { 
-        letters_guessed.push(letter)
-        remaining_guesses--
-      }
-
-      if (remaining_guesses === 0) {
-        word_solved.push(random_word);
-        console.log(word_solved)
-        current_word = random_word.split();
-      }
-    }
-    updateDisplay();
-  };
+  }
   
-  start();
+  if (foundLetter === false) {
+    // Check if inccorrect guess is already on the list
+    if (guessedLetters.includes(letter) === false) {
+      // Add incorrect letter to guessed letter list
+      guessedLetters.push(letter)
+      // Decrement the number of remaining guesses
+      numGuess--
+    }
+    if (numGuess === 0) {
+      // Add word to usedGuessingWords array to not be repeated
+      usedGuessingwWords.push(wordToMatch);
+      console.log(usedGuessingwWords)
+      // Display word before reseting game
+      guessingWord = wordToMatch.split();
+      pause = true;
+      setTimeout(resetGame, 2000);
+    }
+  }
+  updateDisplay();
+};
+
+initializeGame();
